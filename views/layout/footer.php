cc60754e6a821a19bac9eec9a1ad23c4f1e1b515
Jacobo-Dominguez <jacobo9810@gmail.com>
2025-12-05 16:44:33 +0100

    Borrado de archivos para posterior subida

--- views/layout/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-<footer class="site-footer">
-    <div class="container">&copy; <?php echo date('Y'); ?> BlogPersonal</div>
-</footer>
-
-<script src="/public/assets/js/theme.js"></script>
-</body>
-</html>
